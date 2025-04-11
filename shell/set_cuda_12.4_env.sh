f9cdf101a565955417e87e1d4835a9a814423a6f
bro_SiN <bro.sin@qq.com>
2025-04-11 16:36:26 +0800

    修正set_cuda_12.4_env.sh脚本的shebang和环境变量设置方式

--- shell/set_cuda_12.4_env.sh
+++ shell/set_cuda_12.4_env.sh
@@ -1,6 +1,6 @@
-#!bin/bash
+#!/bin/sh
 function set_cu124_env(){
-	export PATH=$PATH:/usr/local/cuda-12.4/bin
-	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-12.4/lib64
+	export PATH=${PATH:+${PATH}:}/usr/local/cuda-12.4/bin
+	export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:+${LD_LIBRARY_PATH}:}/usr/local/cuda-12.4/lib64
 }
 set_cu124_env
